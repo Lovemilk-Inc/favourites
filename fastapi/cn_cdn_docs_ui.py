64a6ad4a9ae15bc0fc3f7febe1479d96fb480ccf
lovemilk <zhuhansan666@outlook.com>
2024-05-11 18:42:33 +0800

    add: 使用 CDN 加速 Swagger UI

---
+++ fastapi/cn_cdn_docs_ui.py
@@ -0,0 +1,25 @@
+"""
+使用 CDN 加速 Swagger UI
+"""
+
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi import applications
+
+
+def replace_swagger_ui():
+    def swagger_monkey_patch(*args, **kwargs):
+        """
+        Wrap the function which is generating the HTML for the /docs endpoint and
+        overwrite the default values for the swagger js and css.
+        """
+        kwargs.pop('swagger_js_url', None)
+        kwargs.pop('swagger_css_url', None)
+
+        return get_swagger_ui_html(  # 以免中国大陆无法获取 js css, 以至无法加载页面
+            *args, **kwargs,
+            swagger_js_url="https://cdn.bootcdn.net/ajax/libs/swagger-ui/5.17.3/swagger-ui-bundle.min.js",
+            swagger_css_url="https://cdn.bootcdn.net/ajax/libs/swagger-ui/5.17.3/swagger-ui.min.css")
+
+
+    # Actual monkey patch
+    applications.get_swagger_ui_html = swagger_monkey_patch
